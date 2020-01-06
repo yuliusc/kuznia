b233aa46ed42a6be8c2848d9d88e9e6ce10e1a5a
yuliusc <48122458+yuliusc@users.noreply.github.com>
2020-01-06 14:21:14 +0100

    Update DataRead.py

--- DataRead.py
+++ DataRead.py
@@ -6,3 +6,26 @@ data = []
 for line in file:
     line.split(',')
     data.append(line.split(','))
+    
+    
+
+#bardzo nieprofesjonalna wersja, ale działa    
+import pandas as pd
+import os
+
+headers = []
+dane = []
+if os.path.isfile('diabetic_data_initial.csv'):
+    with open('diabetic_data_initial.csv', "r") as zawartosc:
+        count = 0
+
+        for linia in zawartosc:
+            if (count == 0):
+                headers = linia.split(",")
+                count += 1
+            else:
+                linia = linia.replace("\"", "")
+                dane.append(tuple(linia.split(",")))
+
+data = pd.DataFrame(dane)
+print(data)
